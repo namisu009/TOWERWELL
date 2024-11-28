c2bc6109347115a434a5e7fd670b531ea11a24ad
namheesoo <namisu009@naver.com>
2024-11-29 02:19:45 +0900

    dtd

--- SW설계기초프로젝트/main.cpp
+++ SW설계기초프로젝트/main.cpp
@@ -2,6 +2,52 @@
 #include <io.h>
 #include <fcntl.h>
 
+#include <windows.h>
+#include <iostream>
+
+// 문자 셀 크기
+const int CELL_WIDTH = 3;
+const int CELL_HEIGHT = 5;
+
+// 창 크기와 위치를 설정하는 함수
+void SetConsoleWindowPosition(int cmdWidth, int cmdHeight) {
+    HWND consoleWindow = GetConsoleWindow();
+
+    // 창 크기 계산 (픽셀 단위)
+    int windowWidth = cmdWidth * CELL_WIDTH;
+    int windowHeight = cmdHeight * CELL_HEIGHT;
+
+    // 화면 해상도 가져오기
+    HMONITOR hMonitor = MonitorFromWindow(consoleWindow, MONITOR_DEFAULTTOPRIMARY);
+    MONITORINFO monitorInfo = { sizeof(MONITORINFO) };
+    GetMonitorInfo(hMonitor, &monitorInfo);
+
+    int screenWidth = monitorInfo.rcMonitor.right - monitorInfo.rcMonitor.left;
+    int screenHeight = monitorInfo.rcMonitor.bottom - monitorInfo.rcMonitor.top;
+
+    // 창 위치 계산 (화면 중앙에 배치)
+    int posX = (screenWidth - windowWidth) / 2;
+    int posY = (screenHeight - windowHeight) / 2;
+
+    // 창 크기와 위치 설정
+    MoveWindow(consoleWindow, posX, posY, windowWidth, windowHeight, TRUE);
+}
+
+void AdjustConsole(int cmdWidth, int cmdHeight) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // 버퍼 크기와 창 크기 설정
+    COORD bufferSize = { (SHORT)cmdWidth, (SHORT)cmdHeight };
+    SMALL_RECT windowSize = { 0, 0, (SHORT)(cmdWidth - 1), (SHORT)(cmdHeight - 1) };
+
+    SetConsoleScreenBufferSize(hConsole, bufferSize);
+    SetConsoleWindowInfo(hConsole, TRUE, &windowSize);
+
+    // 창 크기와 위치 설정
+    SetConsoleWindowPosition(cmdWidth, cmdHeight);
+}
+
+
 void fullscreen() {
     SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), CONSOLE_FULLSCREEN_MODE, 0);
 }
@@ -14,6 +60,9 @@ void setConsoleEncoding() {
 
 int main() {
     //fullscreen();
+
+    AdjustConsole(cmdWidth, cmdHeight);
+
     setConsoleEncoding();
     GameManager gameManager;
     gameManager.initialize();
